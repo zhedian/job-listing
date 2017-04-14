8a58d0dc2cf90f93d11ce9f19dfc9ac94efe2126
zhedian <cooldeer@126.com>
2017-04-14 20:28:30 +0800

    add admin jobcontroller CRUD

---
+++ app/controllers/admin/jobs_controller.rb
@@ -0,0 +1,54 @@
+class Admin::JobsController < ApplicationController
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_job , only: [:show, :edit, :update, :destroy]
+  def index
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to admin_jobs_path
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @job.update(job.params)
+      redirect_to admin_jobs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @job.destroy
+      redirect_to admin_jobs_path
+    end
+  end
+
+
+
+  private
+  def find_job
+    @job = Job.find(params[:id])
+  end
+  def job_params
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+  end
+
+
+end
